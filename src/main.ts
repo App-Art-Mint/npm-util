465bccea5e889262365944d32f80392fba59a30d
Sam Underwood <sam@sunderapps.com>
2022-07-10 01:07:36 -0400

    Added main.ts

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * A utility library for web applications.
+ * 
+ * @packageDocumentation
+ */
+
+/**
+ * Imports
+ */
+ import sunSelectors from './ts/selectors';
+ import sunSettings from './ts/settings';
+ import sunUtil from './ts/util';
+ 
+ /**
+  * Exports
+  */
+ export { 
+     sunSelectors,
+     sunSettings,
+     sunUtil
+ };
+ export default sunUtil;
